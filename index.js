3e00681e126e5c106a44db6c65322e935ec1a2e8
Ahmed kamil jemil <akjemil@gmail.com>
2018-09-03 07:37:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,13 +24,14 @@ function calculatesFarePrice(A,B){
   var Blocks=(A>B)?A-B:B-A;
   var feet=Blocks*264
   if (feet<=264){
-    return '0'
+    return 0
   }
   else if (feet>400 && feet<2000) {
-    return '2.56'
+    return 2.56
   }
   else if(feet>2000&& feet<2500){
-    
+    return 25
   }
+  else return 'cannot travel that far'
 }
  
